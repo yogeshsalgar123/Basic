4ce2ea78c8d85ae3ad0779564fcbc1c3498a915b
yogeshsalgar123 <121169156+yogeshsalgar123@users.noreply.github.com>
2023-05-09 11:09:27 +0530

    Add files via upload

---
+++ BusCarRoad/Road.java
@@ -0,0 +1,12 @@
+package BusCarRoad;
+
+public class Road {
+	public static void main(String[] args)
+	{
+		Truck t=new Truck("Brown",8,"Heavy Goods");
+		Bus b=new Bus("Green",6,"Mini");
+		Car c=new Car("White",4,"Tesla");
+		System.out.println("");
+	}
+
+}
